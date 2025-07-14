4198e3ce83d3070a752c23d58c09a75d7284cef4
ankitsinghbash <ankitsingh4734414code@gmail.com>
2025-07-14 12:38:28 +0530

    added forntend url

--- index.js
+++ index.js
@@ -10,15 +10,46 @@ const port = 5000;
 const app = express();
 
 
+
+
+
+
+
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://phdashboard-frontend.vercel.app/',
+];
+
+// Apply CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
+
+// Optional: handle preflight for all routes
+app.options('*', cors());
+
+
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
 
 app.use(bodyParser.json());
 
 
-
+//https://phdashboard-backend-1.onrender.com   //backend url
+//https://phdashboard-frontend.vercel.app/       //frontend url
 
 // Add cookie parser middleware
 app.use(cookieParser());
