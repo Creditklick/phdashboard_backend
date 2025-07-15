61d192821866b4dedc61b66c5481366da707e200
ankitsinghbash <ankitsingh4734414code@gmail.com>
2025-07-15 11:48:10 +0530

    correct config

--- config/config.js
+++ config/config.js
@@ -7,30 +7,30 @@ const mysql = require('mysql2');
 
 
 
-const Pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'phdashboard',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  multipleStatements: true 
-});
-
-
-
-
-
 // const Pool = mysql.createPool({
-//     host: '68.178.145.55',
-//     user: 'CREDITKLICK_CRM',
-//     password: 'CREDITKLICK_CRM',
-//     database: 'CRM_IMS',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'phdashboard',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   multipleStatements: true 
 // });
+
+
+
+
+
+const Pool = mysql.createPool({
+    host: '68.178.145.55',
+    user: 'CREDITKLICK_CRM',
+    password: 'CREDITKLICK_CRM',
+    database: 'CRM_IMS',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 
 Pool.promise().getConnection()
